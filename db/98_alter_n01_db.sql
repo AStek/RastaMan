cb6b6f4b59b8c668aff284413802d7542fd3ee9e
grans-bwa <grans.bwa@gmail.com>
2011-05-29 20:28:11 +0300

    Preparation to merge with master

--- db/98_alter_n01_db.sql
+++
@@ -1,9 +0,0 @@
-alter table "ACCOUNT" add constraint
-"ACCOUNT_LGN" unique ("LOGIN")
-/
-alter table "RESOURCES" add constraint
-"RESOURCES_TTL" unique ("TITLE")
-/
-alter table "ROLE" add constraint
-"ROLE_TTL" unique ("TITLE")
-/
